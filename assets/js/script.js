22fc00ed5bb1756e4db81022cef5584034e14fd4
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-20 07:24:36 -0500

    Make introudction page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
+var startQuizBtn = document.querySelector("#start-quiz");
 
+startQuizBtn.addEventListener("click", startQuizHandler);
 
+function startQuizHandler(event) {
+    var introPage = document.querySelectorAll(".intro");
+    for (var i = 0; i < introPage.length; i++) {
+        introPage[i].remove();
+    }
+}
