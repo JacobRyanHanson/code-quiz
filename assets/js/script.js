0f5108ddd82208582846f56f9847528f18e85db3
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-21 09:35:44 -0500

    Add questionInfo object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,107 @@
-var main = document.querySelector("main");
-var startQuizBtn = document.querySelector("#start-quiz");
-
-startQuizBtn.addEventListener("click", startQuizHandler);
-main.addEventListener("click", nextQuestion);
-
-function startQuizHandler(event) {
-    var introPage = document.querySelectorAll(".intro");
-    for (var i = 1; i < introPage.length; i++) {
-        introPage[i].remove();
-    }
-    questions(introPage[0]);
-}
-
-function questions(title) {
-    var questionNumber = 1;
-    var question = title;
-    question.className = "question";
-    questionArray = [
+var questionInfo = {
+    number: 1,
+    ask: [
         "Which of the following is not a commonly used data type?",
-        "What condidiotn is used to close if / else statements?",
+        "What condidition is used to close if / else statements?",
         "Arrays is JavaScript can be used to store which of the following?",
         "String values require which of the following to prevent syntax errors when initalized?",
-        "In JavaScript what tool is used to print information (typically for debugging)?",
-        "Complete!"
-    ]
-    question.textContent = questionArray[questionNumber];
-    answers(questionNumber);
-    questionNumber++;
-}
-
-function answers(questionNumber) {
-    var questionChoices = [
+        "In JavaScript what tool is used to print information (typically for debugging)?"
+    ],
+    answer: [
         ["Strings", "Booleans", "Alerts", "Numbers"],
         ["Quotes", "Braces", "Parenthesis", "Brackets"],
         ["Numbers", "Arrays", "Strings", "All of the above"],
         ["Commas", "Braces", "Quotes", "Parenthesis"],
         ["JavaScript", "Terminal", "For Loops", "console.log"]
     ]
+}
 
-    var correctAnswers = ["Alerts", "Braces", "All of the above", "Quotes", "console.log"]
+var main = document.querySelector("main");
+var startQuizBtn = document.querySelector("#start-quiz");
 
+startQuizBtn.addEventListener("click", startQuizHandler);
+
+function startQuizHandler() {
+    var introPage = document.querySelectorAll(".intro");
+    for (var i = 1; i < introPage.length; i++) {
+        introPage[i].remove();
+    }
+    createElements();
+    assignElements();
+
+    quiz();
+}
+
+function quiz() {
+    var btnContainer = document.querySelector(".btnContainer");
+    btnContainer.addEventListener("click", nextQuestionHandler);
+}
+
+function createElements() {
     var btnContainer = document.createElement("div");
     btnContainer.className = "btnContainer";
 
     var btn;
-    var arrayAnswerIndex = questionChoices[questionNumber - 1];
-    for (var i = 0; i < arrayAnswerIndex.length; i++) {
-        btn = document.createElement("button")
-        btn.textContent = arrayAnswerIndex[i];
+    for (var i = 0; i < questionInfo.answer[questionInfo.number - 1].length; i++) {
+        btn = document.createElement("button");
+        btn.className = "answer";
 
-        var isCorrect = isBtnCorrect(arrayAnswerIndex[i], correctAnswers);
-        if (isCorrect) {
-            btn.setAttribute("data-answer-id", true);
-        } else if (!isCorrect) {
-            btn.setAttribute("data-answer-id", false);
-        }
+
+        console.log(btn)
+
+
         btnContainer.appendChild(btn);
     }
     main.appendChild(btnContainer);
+
+    var feedbackContainer = document.createElement("div");
+    var feedback = document.createElement("h2");
+    feedback.className = "feedback";
+    feedbackContainer.appendChild(feedback);
+    main.appendChild(feedbackContainer);
 }
 
-function isBtnCorrect(answerBtn, correctAnswers) {
+function assignElements() {
+    var question = document.querySelector("main h1");
+    question.className = "question";
+    question.textContent = questionInfo.ask[questionInfo.number - 1];
+
+    var btns = document.querySelectorAll(".answer");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].textContent = questionInfo.answer[questionInfo.number - 1][i];
+
+        var isCorrect = isBtnCorrect(btns[i].textContent);
+
+        if (isCorrect) {
+            btns[i].setAttribute("data-answer-id", true);
+        } else if (!isCorrect) {
+            btns[i].setAttribute("data-answer-id", false);
+        }
+    }
+}
+
+function isBtnCorrect(btnText) {
+    var correctAnswers = ["Alerts", "Braces", "All of the above", "Quotes", "console.log"]
+
     var isTrue = false;
     for (var i = 0; i < correctAnswers.length; i++) {
-        if (answerBtn === correctAnswers[i]) {
+        if (btnText === correctAnswers[i] && i === questionInfo.number - 1) {
             isTrue = true;
         }
     }
     return isTrue;
 }
 
-function nextQuestion(event) {
-    //if ()
+function nextQuestionHandler(event) {
+    if (questionInfo.number - 1 < questionInfo.ask.length - 1) {
+        questionInfo.number++;
+    }
+    assignElements();
+
+    var feedback = document.querySelector(".feedback");
+    if (event.target.matches("[data-answer-id='true'")) {
+        feedback.textContent = "Correct"
+    } else if (event.target.matches("[data-answer-id='false'")) {
+        feedback.textContent = "Incorrect"
+    }
 }
